00a1f8ebd4663d55cb83f3702ec3a0326d0dd678
ozzyag9-cloud <ozzyag9@gmail.com>
2026-04-30 14:45:50 +0400

    Update YouTube.tsx

--- apps/web/src/app/components/player/YouTube.tsx
+++ apps/web/src/app/components/player/YouTube.tsx
@@ -3,8 +3,10 @@ export default function YouTube({ id }: { id: string }) {
     <iframe
       width="100%"
       height="200"
-      src={`https://www.youtube.com/embed/${id}`}
+      src={`https://www.youtube.com/embed/${id}?modestbranding=1`}
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
+      style={{ border: "none" }}
     />
   );
 }
